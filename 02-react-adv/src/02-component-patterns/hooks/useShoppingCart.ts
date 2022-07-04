a14e198cfaa6f305dbf93c55807b99e52f52e1b8
juan.acuna <juan.acuna@unosquare.com>
2022-07-04 09:58:47 -0500

    Control Props alternativo

--- 02-react-adv/src/02-component-patterns/hooks/useShoppingCart.ts
+++ 02-react-adv/src/02-component-patterns/hooks/useShoppingCart.ts
@@ -13,51 +13,19 @@ export const useShoppingCart = () => {
     count: number;
     product: Product;
   }) => {
-    // Así lo hace Fernando Herrera
-    // setShoppingCart((oldShoppingCart) => {
-    //   if (!count) {
-    //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-    //     console.log(toDelete);
-    //     return rest;
-    //   }
-    //   return {
-    //     ...oldShoppingCart,
-    //     [product.id]: {
-    //       ...product,
-    //       count,
-    //     },
-    //   };
-    // });
-
-    // // Mi solución creo que es más limpia
-    // if (!count) {
-    //   delete shoppingCart[product.id];
-    //   setShoppingCart({ ...shoppingCart });
-    //   return;
-    // }
-    // setShoppingCart({
-    //   ...shoppingCart,
-    //   [product.id]: {
-    //     ...product,
-    //     count,
-    //   },
-    // });
-
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCard = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      if (!count) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        console.log(toDelete);
+        return rest;
       }
-      // borrar el producto
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return { ...rest };
+      return {
+        ...oldShoppingCart,
+        [product.id]: {
+          ...product,
+          count,
+        },
+      };
     });
   };
   return { shoppingCart, onProductCountChange };
