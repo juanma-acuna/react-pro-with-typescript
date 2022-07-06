d940d4c4166ec83b4dd56bc802b7c5ff3c51878a
juan.acuna <juan.acuna@unosquare.com>
2022-07-06 12:40:54 -0500

    Consumo paquete minor modifications

--- 02-react-adv/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 02-react-adv/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -6,8 +6,6 @@ import {
 } from "../components";
 import { products } from "../data/products";
 
-import "../styles/custom-styles.css";
-
 const product = products[0];
 
 const ShoppingPage = () => {
@@ -17,29 +15,17 @@ const ShoppingPage = () => {
       <hr />
       <ProductCard
         product={product}
-        className="bg-color text-white"
         key={product.id}
         initialValues={{
-          count: 4,
-          maxCount: 10,
+          count: 0,
+          // maxCount: 10,
         }}
       >
         {(args) => (
           <>
-            <ProductImage
-              className="custom-image"
-              style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
-            />
-            <ProductTitle className="text-white text-bold" />
-            <ProductButtons className="custom-buttons" />
-            <button onClick={args.reset}>Reset</button>
-            <button onClick={() => args.increaseBy(-2)}>-2</button>
-            {!args.isMaxCountReached && (
-              <button onClick={() => args.increaseBy(2)}>+2</button>
-            )}
-            <span>
-              {args.count} - {args.maxCount}
-            </span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
